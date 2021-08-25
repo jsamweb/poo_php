d30f8b719eff7b187f36f2adbee15c33a5924bb0
jsamweb <jsam.web@gmail.com>
2021-08-25 12:49:08 +0200

    alcance

---
+++ alcance.php
@@ -0,0 +1,20 @@
+<?php
+
+use PhpMyAdmin\Server\UserGroups;
+
+class User
+{
+    //public
+    //protected
+    //private
+    public const PAGINATE = 25;
+
+    public $username;
+    // protected $username;
+    // private $username;
+    public function getUserName()
+    {
+        //...
+    }
+
+}
